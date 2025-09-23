dd6c37b25ad826a0a5fbce1eb456c84eb88d214d
Bastian142000 <bastian.prado.leiva@gmail.com>
2025-09-22 21:16:23 -0300

    Styles(layout): Apply new styles and organization layout

--- src/ui/ProtectedPage.jsx
+++ src/ui/ProtectedPage.jsx
@@ -8,21 +8,18 @@ export default function ProtectedPage() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div>
-      <div className="relative flex h-screen flex-col">
+    <div className="app-container">
+      <div className="navbar">
         <Navbar />
-        <div className="flex flex-1">
-          {isLoading && (
-            <div className="absolute top-6/12 right-6/12">
-              <CircularProgress color="secondary" />
-            </div>
-          )}
-          <Sidebar />
-          <main className="flex-1 overflow-y-auto p-6">
-            <Outlet />
-          </main>
-        </div>
       </div>
+
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+
+      <main className="main">
+        <Outlet />
+      </main>
     </div>
   );
 }
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,88 +1,67 @@
-import MenuIcon from "@mui/icons-material/Menu";
+import { NavLink } from "react-router";
 import HomeIcon from "@mui/icons-material/Home";
 import ArchiveIcon from "@mui/icons-material/Archive";
 import ClassIcon from "@mui/icons-material/Class";
-import { useState } from "react";
-import { NavLink } from "react-router";
 
 export default function Sidebar() {
-  const [isOpen, setIsOpen] = useState(true);
-
   return (
-    <div
-      className={`${
-        isOpen ? "w-60" : "w-15"
-      } relative max-h-lvh border-r-1 border-gray-200 shadow-sm transition-all duration-300 ease-in-out`}
-    >
-      {/* Sidebar button */}
-      <div className="absolute top-5 right-5">
-        <button onClick={() => setIsOpen(!isOpen)}>
-          <MenuIcon className="cursor-pointer text-gray-500 transition duration-300 ease-in-out hover:text-blue-500" />
-        </button>
-      </div>
+    <div className="h-screen border-r border-gray-200 shadow-md transition-all duration-500 ease-in-out">
+      <ul className="flex h-screen flex-col justify-center gap-10">
+        {/* Logo + Subtítulo */}
+        <li className="mb-6 flex flex-col items-center opacity-0 md:opacity-100">
+          <img
+            className="h-24 w-24 cursor-pointer rounded-full border border-gray-300 object-cover shadow-md"
+            src="../logo.png"
+            alt="Logo"
+          />
+          <span className="mt-3 text-lg font-semibold text-blue-900 italic">
+            MY FINANCIAL SPACE
+          </span>
+        </li>
 
-      {/* Links */}
-      <div className="flex flex-col gap-20 pt-30 pl-4 text-sm">
-        <div className="text-gray-800 transition ease-in-out hover:text-blue-500">
+        {/* Links */}
+        <li className="text-gray-800 hover:text-blue-500">
           <NavLink
             to={"/app/dashboard"}
             className={({ isActive }) =>
-              `flex items-center ${isActive ? "rounded-l-2xl bg-gradient-to-r from-blue-200 to-blue-600 py-2" : ""}`
+              `flex items-center transition-colors duration-300 ease-in-out ${isActive ? "rounded-l-2xl bg-gradient-to-r from-blue-200 to-blue-600 py-2" : ""}`
             }
           >
-            <HomeIcon className="mr-4" />
-            <span
-              className={`inline-block origin-left transform transition-all duration-500 ease-in-out ${
-                isOpen
-                  ? "ml-0 scale-x-100 opacity-100"
-                  : "ml-[-8px] scale-x-0 opacity-0"
-              }`}
-            >
+            <HomeIcon fontSize="large" className="mr-4 ml-4" />
+            <span className="inline-block origin-left transform text-xl font-semibold uppercase opacity-0 md:opacity-100">
               Home
             </span>
           </NavLink>
-        </div>
+        </li>
 
-        <div className="text-gray-800 transition ease-in-out hover:text-blue-500">
+        <li className="text-gray-800 hover:text-blue-500">
           <NavLink
             to={"/app/transactions"}
             className={({ isActive }) =>
-              `flex items-center ${isActive ? "rounded-l-2xl bg-gradient-to-r from-blue-200 to-blue-600 py-2" : ""}`
+              `flex items-center transition-colors duration-300 ease-in-out ${isActive ? "rounded-l-2xl bg-gradient-to-r from-blue-200 to-blue-600 py-2" : ""}`
             }
           >
-            <ArchiveIcon className="mr-4" />
-            <span
-              className={`inline-block origin-left transform transition-all duration-500 ease-in-out ${
-                isOpen
-                  ? "ml-0 scale-x-100 opacity-100"
-                  : "ml-[-8px] scale-x-0 opacity-0"
-              }`}
-            >
+            <ArchiveIcon fontSize="large" className="mr-4 ml-4" />
+            <span className="inline-block origin-left transform text-xl uppercase opacity-0 md:opacity-100">
               Add new transactions
             </span>
           </NavLink>
-        </div>
+        </li>
 
-        <div className="text-gray-800 transition ease-in-out hover:text-blue-500">
+        <li className="text-gray-800 hover:text-blue-500">
           <NavLink
             to={"/app/categories"}
             className={({ isActive }) =>
-              `flex items-center ${isActive ? "rounded-l-2xl bg-gradient-to-r from-blue-200 to-blue-600 py-2" : ""}`
+              `flex items-center transition-colors duration-300 ease-in-out ${isActive ? "rounded-l-2xl bg-gradient-to-r from-blue-200 to-blue-600 py-2" : ""}`
             }
           >
-            <ClassIcon className="mr-4" />
-            <span
-              className={`inline-block origin-left transform transition-all duration-500 ease-in-out ${
-                isOpen
-                  ? "ml-0 scale-x-100 opacity-100"
-                  : "ml-[-8px] scale-x-0 opacity-0"
-              }`}
-            >
+            <ClassIcon fontSize="large" className="mr-4 ml-4" />
+            <span className="inline-block origin-left transform text-xl uppercase opacity-0 ease-in-out md:opacity-100">
               Categories
             </span>
           </NavLink>
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
   );
 }
